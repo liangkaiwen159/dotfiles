4c3c7af679f70fb8abb866f8a81472a93f5577d8
liangkaiwen159 <82089963+liangkaiwen159@users.noreply.github.com>
2025-03-28 14:04:09 +0800

    Update ram_info.sh

--- tmux/.tmux/plugins/tmux/scripts/ram_info.sh
+++ tmux/.tmux/plugins/tmux/scripts/ram_info.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# setting the locale, some users have issues with different locales, this forces the correct one
+# 设置区域信息，确保使用正确的语言环境
 export LC_ALL=en_US.UTF-8
 
 current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -9,15 +9,26 @@ get_ratio()
 {
   case $(uname -s) in
     Linux)
-      usage="$(free -h | awk 'NR==2 {print $3}')"
-      total="$(free -h | awk 'NR==2 {print $2}')"
-      formated="${usage}/${total}"
+      # 提取内存信息
+      mem_info=$(free -m | awk 'NR==2 {print $3, $7, $2}')
+      # 解析数值
+      read used available total <<< "$mem_info"
       
-      echo "${formated//i/B}"
+      # 计算实际使用的内存（总内存减去可用内存）
+      adjusted_used=$((total - available))
+
+      # 格式化输出
+      if ((adjusted_used < 1024)); then
+        echo "${adjusted_used}MB/${total}MB"
+      else
+        adjusted_used_gb=$((adjusted_used / 1024))
+        total_gb=$((total / 1024))
+        echo "${adjusted_used_gb}GB/${total_gb}GB"
+      fi
       ;;
 
     Darwin)
-      # Get used memory blocks with vm_stat, multiply by page size to get size in bytes, then convert to MiB
+      # macOS的内存使用情况代码
       used_mem=$(vm_stat | grep ' active\|wired ' | sed 's/[^0-9]//g' | paste -sd ' ' - | awk -v pagesize=$(pagesize) '{printf "%d\n", ($1+$2) * pagesize / 1048576}')
       total_mem=$(system_profiler SPHardwareDataType | grep "Memory:" | awk '{print $2 $3}')
       if ((used_mem < 1024 )); then
@@ -29,7 +40,7 @@ get_ratio()
       ;;
 
     FreeBSD)
-      # Looked at the code from neofetch
+      # FreeBSD的内存使用情况代码
       hw_pagesize="$(sysctl -n hw.pagesize)"
       mem_inactive="$(($(sysctl -n vm.stats.vm.v_inactive_count) * hw_pagesize))"
       mem_unused="$(($(sysctl -n vm.stats.vm.v_free_count) * hw_pagesize))"
@@ -38,7 +49,6 @@ get_ratio()
       free_mem=$(((mem_inactive + mem_unused + mem_cache) / 1024 / 1024))
       total_mem=$(($(sysctl -n hw.physmem) / 1024 / 1024))
       used_mem=$((total_mem - free_mem))
-      echo $used_mem
       if ((used_mem < 1024 )); then
         echo "${used_mem}MB/$total_mem"
       else
@@ -48,8 +58,7 @@ get_ratio()
       ;;
 
     OpenBSD)
-      # vmstat -s | grep "pages managed" | sed -ne 's/^ *\([0-9]*\).*$/\1/p'
-      # Looked at the code from neofetch
+      # OpenBSD的内存使用情况代码
       hw_pagesize="$(pagesize)"
       used_mem=$(( ( 
 $(vmstat -s | grep "pages active$" | sed -ne 's/^ *\([0-9]*\).*$/\1/p') +
@@ -58,7 +67,6 @@ $(vmstat -s | grep "pages wired$" | sed -ne 's/^ *\([0-9]*\).*$/\1/p') +
 $(vmstat -s | grep "pages zeroed$" | sed -ne 's/^ *\([0-9]*\).*$/\1/p') +
 0) * hw_pagesize / 1024 / 1024 ))
       total_mem=$(($(sysctl -n hw.physmem) / 1024 / 1024))
-      #used_mem=$((total_mem - free_mem))
       total_mem=$(($total_mem/1024))
       if (( $used_mem < 1024 )); then
         echo $used_mem\M\B/$total_mem\G\B
@@ -69,7 +77,7 @@ $(vmstat -s | grep "pages zeroed$" | sed -ne 's/^ *\([0-9]*\).*$/\1/p') +
       ;;
 
     CYGWIN*|MINGW32*|MSYS*|MINGW*)
-      # TODO - windows compatability
+      # TODO - Windows兼容性
       ;;
   esac
 }
@@ -81,5 +89,5 @@ main()
   echo "$ram_label $ram_ratio"
 }
 
-#run main driver
+# 运行主驱动程序
 main
